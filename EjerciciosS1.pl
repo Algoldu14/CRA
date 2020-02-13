a910287194b00a6e6e3bf85da121549c08ba97f0
Algoldu14 <alvarogolbanoduran@gmail.com>
2020-02-13 09:04:44 +0100

    Juego de Soldados empezado

--- EjerciciosS1.pl
+++ EjerciciosS1.pl
@@ -28,3 +28,35 @@ sumar_1_aux(ListaNums,ResAux,Resultados):-
             
 sumar_1_aux([],Resultados,Resultados).
 sumar_1([]).
+
+hermanos(Persona,ListaAux,ListaHermanos):- % Devuelve la lista de hermanos de Persona
+         hermano(Persona,Hermano),
+         \+ member(Hermano,ListaAux),!, % Para que el hermano no pertenezca a la lista, si ocurre hace backtraking  ,!, corta el backtraking
+         append(ListaAux,[Hermano],ListaAux2),
+         hermanos(Persona,ListaAux2,ListaHermanos).
+         
+hermanos(_,ListaHermanos,ListaHermanos). % Esto cierra la recursividad, el _ implica que nos da igual el valor de esa variable
+
+
+%--------------------------- Sesion 3 --------------------------------------%
+
+pedir_numero(Min,Max,Out):-
+     repeat,   %Se repite hasta que se acierte
+     write('Introduce un numero entre'),
+     write(Min),write(' y '),write(Max),write(':'),
+     read(Out),
+     (Out >= Min, Out =< Max, !).
+     
+simbolos([a,b,c,b,a]).
+
+% simbolos(X),select(b,X,Out). % Elimina un elelemto de la lista X
+% simbolos(X),select(b,X,x,Out). % Sustituye b por x en la lista X
+% simbolos(X),nth0(3,X,Out). % Retorna el tercer elemento de la lista X
+% simbolos(X),nth0(3,X,Out,Res). % Retorna el tercer elemento de la lista X y el resto de la lista sin el parametro 3
+
+listas_simbolos([[a,a,b,b],[c,c,d,c],[a,b,c,d]]).
+
+reemplazar_simbolo_sublista(Lista,Nsubllista,ValorOri,ValorNue,ListaOut):-
+           nth1(Nsublista,Lista,Sublista,Res),
+           select(ValorOri, Sublista, ValorNue, SubListaOut),
+           nth1(Nsublista, ListaOut, SubListaOut, Res).
