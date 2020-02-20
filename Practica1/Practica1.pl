b777edb794804ca3eeac4445f5aa393418b14eb1
Laura Ramos <luis@Luiss-MacBook-Air.local>
2020-02-20 08:41:53 +0100

    Cosas para ejecutar

---
+++ Practica1/Practica1.pl
@@ -0,0 +1,6 @@
+
+
+Bag(['N','N','N','N','N','N','N','N','N','N','A','A','A','A','A','A','A','A','A','A','B','B','B','B','B','B','B','B','B','B','R','R','R','R','R','R','R','R','R','R','V','V','V','V','V','V','V','V','V','V'])
+
+?- random_permutation(Bag,BagS).
+
