6a9993a2d5c715e642f0d2f37adfe50cffb8fbbf
Algoldu14 <alvarogolbanoduran@gmail.com>
2020-02-20 08:51:37 +0100

    Funciona

--- Practica1/Practica1.pl
+++ Practica1/Practica1.pl
@@ -1,6 +1,4 @@
+bag(['N','N','N','N','N','N','N','N','N','N','A','A','A','A','A','A','A','A','A','A','B','B','B','B','B','B','B','B','B','B','R','R','R','R','R','R','R','R','R','R','V','V','V','V','V','V','V','V','V','V']).
 
-
-Bag(['N','N','N','N','N','N','N','N','N','N','A','A','A','A','A','A','A','A','A','A','B','B','B','B','B','B','B','B','B','B','R','R','R','R','R','R','R','R','R','R','V','V','V','V','V','V','V','V','V','V'])
-
-?- random_permutation(Bag,BagS).
+random_bag(BagS):-bag(Bag),random_permutation(Bag,BagS).
 
