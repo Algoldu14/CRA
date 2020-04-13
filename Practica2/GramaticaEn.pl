409f920ab52a14ce5d002e811595c7d89f2e1ffb
Algoldu14 <alvarogolbanoduran@gmail.com>
2020-04-13 20:16:11 +0200

    Cositas

--- Practica2/GramaticaEn.pl
+++ Practica2/GramaticaEn.pl
@@ -5,11 +5,6 @@ sentence(o(GN,GV,GC)) --> g_noun(GN,_,Num),g_verb(GV,_,Num),g_circus(GC,_,_).
 sentence(o(GN,GV,GN2,GC)) --> g_noun(GN,_,Num),g_verb(GV,_,Num),g_noun(GN2,_,Num),g_circus(GC,_,_).
 sentence(o(GN,GV)) --> g_noun(GN,G,Num),g_verb(GV,G,Num).
 sentence(o(GV,GN)) --> g_verb(GV,_,Num),g_noun(GN,_,Num).
-sentence(o(GN,PR,O1,GV,GN2)) --> g_noun(GN,_,Num),pronoms(PR),sentence(O1),g_verb(GV,_,Num),g_noun(GN2,_,_).
-sentence(o(O1,C,O2)) --> sentence(O1),conjuction(C),sentence(O2).
-
-
-%Grupo g_nominal
 
 g_noun(gn(A,N),G,Num) --> article(A,G,Num),noun(N,G,Num).
 g_noun(gn(NP,NP2),_,Num) --> noun(NP,Num,_),noun(NP2,Num,_).
@@ -22,8 +17,6 @@ g_noun(gn(A,N,P,NP),G,Num) --> article(A,G,Num),noun(NP,Num,_), preposition(P,G)
 
 g_verb(gv(V),_,Num) --> verb(V,Num).
 g_verb(gv(V,AD),G,Num) --> verb(V,Num),adjective(AD,G).
-
-
 
 g_circus(gc(ADV,ADV2),_,_) --> adverb(ADV),adverb(ADV2).
 g_circus(gc(P,A,N),G,Num) --> preposition(P,G), article(A,G,Num),noun(N,G,Num).
